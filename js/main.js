aaf975a1776684a866985837e269f40cd622f9d9
Sarmad Georgie <sarmadmjg@gmail.com>
2017-07-27 17:19:45 +0300

    Add drop shadow

--- js/main.js
+++ js/main.js
@@ -7,21 +7,23 @@
     var header = $("header");
     var complications = $(".techs, .certifications");
     var title = $(".title");
+    var subtitle = $('.job-title');
+
+    var smallScreen = $(window).width() < 768;
 
     $(window).on('load scroll resize', function() {
-        var smallScreen = $(window).width() < 768;
         var scroll = $(document).scrollTop();
         var fullHeight = $(window).height();
 
-        if (! smallScreen) {
-            title.css({'text-align': 'left'});
-        }
-
+        // Adjust header height
         header.height(Math.max(fullHeight - scroll, headerHeight));
 
+        // Adjust opacity of complications
         opacity = (Math.max(fullHeight - scroll, opacityBreakPoint) - opacityBreakPoint) / (fullHeight - opacityBreakPoint);
         complications.css({'opacity': opacity});
+        subtitle.css({'opacity': opacity});
 
+        // Align title to left/center in small screens
         if (scroll >= opacityBreakPoint) {
             complications.css({'display': 'none'});
             title.css({'text-align': 'left'});
@@ -30,6 +32,24 @@
             if (smallScreen) {
                 title.css({'text-align': 'center'});
             }
+        }
+
+        // drop shadow
+        if (header.height() <= headerHeight) {
+            header.css({'box-shadow': '2px 2px 3px #ccc'});
+        } else {
+            header.css({'box-shadow': 'none'});
+        }
+    });
+
+    // recaluculate screen size check after resizing the window
+    $(window).on('load resize', function() {
+        smallScreen = $(window).width() < 768;
+        if (! smallScreen) {
+            title.css({'text-align': 'left'});
+            headerHeight = 100;
+        } else {
+            headerHeight = 70;
         }
     });
 })();
